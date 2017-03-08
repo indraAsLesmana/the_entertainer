2bfe938c17c8a7b7d2676e4b681a582101780235
Indra Agus Lesmana <indra.agus@kiranatama.com>
2017-03-09 04:15:15 +0700

    matching icon on home button

--- app/src/main/java/com/emveep/theentertainer/HomeActivity.java
+++ app/src/main/java/com/emveep/theentertainer/HomeActivity.java
@@ -20,11 +20,17 @@ public class HomeActivity extends AppCompatActivity {
                 case R.id.navigation_home:
                     mTextMessage.setText(R.string.title_home);
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
                 case R.id.navigation_notifications:
                     mTextMessage.setText(R.string.title_notifications);
+                    return true;
+                case R.id.navigation_favorite:
+                    mTextMessage.setText(R.string.title_favorite);
+                    return true;
+                case R.id.navigation_profile:
+                    mTextMessage.setText(R.string.title_profile);
+                    return true;
+                case R.id.navigation_shoping:
+                    mTextMessage.setText(R.string.title_shop);
                     return true;
             }
             return false;
