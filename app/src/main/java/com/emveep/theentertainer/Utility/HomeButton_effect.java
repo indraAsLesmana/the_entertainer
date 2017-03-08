9c9bc4e0dd23ec02265aa10ecc3ef884282f981b
Indra Agus Lesmana <indra.agus@kiranatama.com>
2017-03-09 05:17:49 +0700

    fix home menu button, with no title

--- app/src/main/java/com/emveep/theentertainer/Utility/HomeButton_effect.java
+++ app/src/main/java/com/emveep/theentertainer/Utility/HomeButton_effect.java
@@ -3,7 +3,11 @@ package com.emveep.theentertainer.Utility;
 import android.support.design.internal.BottomNavigationItemView;
 import android.support.design.internal.BottomNavigationMenuView;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v7.widget.AppCompatImageView;
 import android.util.Log;
+import android.view.Gravity;
+import android.widget.FrameLayout;
+
 import java.lang.reflect.Field;
 
 
@@ -25,11 +29,20 @@ public class HomeButton_effect {
             for (int i = 0; i < menuView.getChildCount(); i++) {
                 BottomNavigationItemView item = (BottomNavigationItemView) menuView.getChildAt(i);
                 //noinspection RestrictedApi
-                item.setShiftingMode(false);
+                item.setShiftingMode(true);
                 // set once again checked value, so view will be updated
                 //noinspection RestrictedApi
                 item.setChecked(item.getItemData().isChecked());
+
+                /**
+                 * how to disable title links:
+                 * http://stackoverflow.com/questions/40183239/remove-bottomnavigationview-labels
+                 * */
+               /* AppCompatImageView icon = (AppCompatImageView) item.getChildAt(0);
+                FrameLayout.LayoutParams params = (FrameLayout.LayoutParams) icon.getLayoutParams();
+                params.gravity = Gravity.CENTER;*/
             }
+
         } catch (NoSuchFieldException e) {
             Log.e("BNVHelper", "Unable to get shift mode field", e);
         } catch (IllegalAccessException e) {
