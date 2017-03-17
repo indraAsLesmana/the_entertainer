0c3d9937845458777cbd9cc24d75d7be23de7128
Indra Agus Lesmana <indra.agus@kiranatama.com>
2017-03-17 07:21:50 +0700

    cant view <include> tag on constraintlayout, add toolbar instade to view.

--- app/src/main/java/com/emveep/theentertainer/Activity/RegisterActivity.java
+++ app/src/main/java/com/emveep/theentertainer/Activity/RegisterActivity.java
@@ -4,13 +4,17 @@ import android.app.Activity;
 import android.content.Intent;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.emveep.theentertainer.Adapter.RegisterFragment_Adapter;
 import com.emveep.theentertainer.R;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
 
     public static void start(Activity caller) {
         Intent intent = new Intent(caller, RegisterActivity.class);
@@ -21,6 +25,15 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        toolbar = (Toolbar) findViewById(R.id.registery_toolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBar action = getSupportActionBar();
+        if (action != null){
+            action.setDisplayShowTitleEnabled(false);
+            action.setDisplayHomeAsUpEnabled(true);
+        }
 
         ViewPager mMain_Pagger = (ViewPager) findViewById(R.id.main_pagger);
 
