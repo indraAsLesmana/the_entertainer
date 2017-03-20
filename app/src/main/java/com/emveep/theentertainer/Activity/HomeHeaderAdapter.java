e59bcaddafd6bb0ffb5a402e7a8327f787db8f25
Indra Agus Lesmana <indra.agus@kiranatama.com>
2017-03-20 15:16:26 +0700

    home view on progress

---
+++ app/src/main/java/com/emveep/theentertainer/Activity/HomeHeaderAdapter.java
@@ -0,0 +1,26 @@
+package com.emveep.theentertainer.Activity;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by indraaguslesmana on 3/20/17.
+ */
+
+public class HomeHeaderAdapter extends FragmentPagerAdapter {
+
+    public HomeHeaderAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+}
